6c82e17e2171835ef4f4858942d8314c163ab8f1
Alline Marjorie <alline.marjorie@gmail.com>
2019-08-30 12:44:49 -0300

    feat(setup): add global Node packages

---
+++ setup/all/node-global-packages.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+npm install -g \
+  yarn \
+  eslint tslint \
+  typescript \
+  node-sass \
+  gulp-cli \
+  webpack-cli \
+  parcel-bundler
+  bower \
+  react-scripts \
+  electron \
+  pm2
