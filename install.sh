5b27d1d6804526f675ad5c09691b1bfd30eee529
Alline Marjorie <allinemarjorie@gmail.com>
2019-08-30 19:16:19 -0300

    feat: add global gitignore

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 
-rm -f ~/.gitconfig ~/.zshrc
+rm -f ~/.gitconfig ~/.zshrc ~/.gitignore
 ln -s $(pwd)/.gitconfig ~/.gitconfig 
 ln -s $(pwd)/.zshrc ~/.zshrc 
+ln -s $(pwd)/.gitconfig ~/.gitconfig
