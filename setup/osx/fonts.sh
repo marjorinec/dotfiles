fd21051b61b3596b47f71f6d2f704b5b21a98a82
Alline Marjorie <allinemarjorie@gmail.com>
2019-08-30 19:24:20 -0300

    feat(setup/osx): add font installers via homebrew

---
+++ setup/osx/fonts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+brew tap homebrew/cask-fonts
+
+brew cask install \
+  font-fira-code \
+  font-fira-mono-for-powerline \
+  font-inconsolata \
+  font-lato \
+  font-liberation-sans \
+  font-ubuntu \
+  font-lora
