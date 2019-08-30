31dc873da4db40ebce1341a347f6f14304a4ba02
Alline Marjorie <allinemarjorie@gmail.com>
2019-08-30 19:15:02 -0300

    feat(setups/osx): add homebrew installation

--- setup/osx/base.sh
+++ setup/osx/base.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+# installs homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
 brew cask install java spectacle flycut visual-studio-code telegram google-chrome
